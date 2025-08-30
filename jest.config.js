2a52b60795b7af60b73d8cb61324df4ee526b2c0
Ahmed Raza <126920056+dev-ahmedrazaturk@users.noreply.github.com>
2025-08-30 22:00:05 +0000

    ci workflow

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  maxWorkers: 1, // Disable parallelism temporarily to avoid the error
+  testEnvironment: 'node', // Use Node environment for tests
+  verbose: true, // Make Jest show individual test results
+};
